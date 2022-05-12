9a97a76f1f8736991480be2cba8da0fbe3a0af3c
Anna R <rozmarin_77@mail.ru>
2022-05-12 15:14:51 +0300

    fixed

---
+++ NodeJS/Algoritms/Increasenumbers.js
@@ -0,0 +1,15 @@
+function IncreasNums(nums){// меняем цифры в массиве по возрастанию
+    for (i = 0; i < nums.length; i++){
+        for (j = i+1; j < nums.length; j++){
+            if(nums[i] > nums[j] ){
+                let temp = nums[i];
+                nums[i] = nums[j];
+              //  console.log(nums[i], nums[j])
+                nums[j] = temp;
+            }
+        }
+    }s
+    return nums;
+}
+console.log('res',IncreasNums([4,3,2,8]));
+
